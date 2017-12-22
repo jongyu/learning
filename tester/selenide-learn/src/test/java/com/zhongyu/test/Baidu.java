754ab3a83f1b8dbc3449e7f894f57010c52bb9e5
ZhongYu <zhongyu@kid17.com>
2017-12-22 16:17:59 +0800

    fix

--- tester/selenide-learn/src/test/java/com/zhongyu/test/Baidu.java
+++ tester/selenide-learn/src/test/java/com/zhongyu/test/Baidu.java
@@ -1,12 +1,16 @@
 package com.zhongyu.test;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.open;
 
 public class Baidu {
 
     @Test
     public void search() {
-
+        Configuration.browser = "chrome";
+        open("https://www.baidu.com");
     }
 
 }
