f2c39e5c27882ab02a51b206647cd47ff4cfac9d
ZhongYu <zhongyu@kid17.com>
2017-12-25 12:06:11 +0800

    update baidu.py

--- tester/selenium-learn-python/Baidu.py
+++ tester/selenium-learn-python/Baidu.py
@@ -3,3 +3,5 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://www.baidu.com")
+driver.find_element_by_name("wd").send_keys("钟宇的博客")
+driver.find_element_by_id("su").click()
