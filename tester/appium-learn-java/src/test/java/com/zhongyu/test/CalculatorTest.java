a19a3d2acaf85fc38782513d1c02066f65a461a2
Aimee <zhongyu54321@gmail.com>
2018-01-25 15:25:04 +0800

    add calculator test

--- tester/appium-learn-java/src/test/java/com/zhongyu/test/CalculatorTest.java
+++ tester/appium-learn-java/src/test/java/com/zhongyu/test/CalculatorTest.java
@@ -1,5 +1,51 @@
 package com.zhongyu.test;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 public class CalculatorTest {
+
+    private AndroidDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "HUAWEI");
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        capabilities.setCapability("unicodeKeyboard", true);
+        capabilities.setCapability("resetKeyboard", true);
+        capabilities.setCapability("appPackage", "com.android.calculator2");
+        capabilities.setCapability("appActivity", ".Calculator");
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void test() {
+        driver.findElementById("digit_7").click();
+        driver.findElementById("op_mul").click();
+        driver.findElementById("digit_8").click();
+        driver.findElementById("eq").click();
+
+        String result = driver.findElement(By.id("formula")).getText();
+        System.out.println("7 * 8 = " + result);
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        Thread.sleep(3000);
+        driver.quit();
+    }
 
 }
