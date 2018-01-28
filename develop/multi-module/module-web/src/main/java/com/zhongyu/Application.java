ac8c11b9de62bce7fc9634fc19396f65ded2e629
Aimee <zhongyu54321@gmail.com>
2018-01-28 10:03:38 +0800

    add multi module project

---
+++ develop/multi-module/module-web/src/main/java/com/zhongyu/Application.java
@@ -0,0 +1,13 @@
+package com.zhongyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
